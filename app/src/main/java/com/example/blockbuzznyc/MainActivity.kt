d2fbf3570e62e66ac8f2164d9372f7d94d69b1d2
kcho760 <kcho760@gmail.com>
2023-12-06 16:13:13 -0500

    First commit

---
+++ app/src/main/java/com/example/blockbuzznyc/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.blockbuzznyc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.blockbuzznyc.ui.theme.BlockBuzzNYCTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BlockBuzzNYCTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello, $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BlockBuzzNYCTheme {
+        Greeting("Android")
+    }
+}
